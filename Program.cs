c2f06822475ea9168bb8102ce079279fbf978cb0
Lurrique <itsmelurrique@gmail.com>
2021-05-31 18:38:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Qual operação deseja fazer? ");
+            char op = Console.ReadKey(true).KeyChar;
+            Console.WriteLine(op);
+            if (op == '+')
+            {
+                Console.WriteLine("Seu resultado é: " + (num1 + num2));
+            }
+            else if (op == '-')
+            {
+                Console.WriteLine("Seu resultado é: " + (num1 - num2));
+            }
+            else if (op == '*')
+            {
+                Console.WriteLine("Seu resultado é: " + (num1 * num2));
+            }
+            else if (op == '/')
+            {
+                Console.WriteLine("Seu resultado é: " + (num1 / num2));
+            }
+            else Console.WriteLine("Você não escolheu uma operação válida");
+
+
+
+
+
+
+
+        }
+       
+    }
+} 
